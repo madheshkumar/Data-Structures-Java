6e2040c61fbaa973346807702874470dc7581874
madhesh kumar <madheshkumars03@gmail.com>
2023-06-05 22:18:16 +0530

    update

---
+++ src/Array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Array {
+    public static void main(String[] args) {
+        int[] num = new int[5];
+        num[0]=11;
+        num[1]=12;
+        String[] colors= new String[5];
+        colors[0]="red";
+        System.out.println(Arrays.toString(num));
+        System.out.println(Arrays.toString(colors));
+    }
+}
---
+++ src/MyStack.java
@@ -0,0 +1,82 @@
+import java.util.*;
+public class MyStack {
+    int size;
+    int stack[];
+    int top;
+    //create stack
+    MyStack(int size){
+        this.size=size;
+        this.stack = new int[size];
+        this.top=-1;
+    }
+
+    public void push(int element){
+        if(!isFull()){
+        top++;
+        stack[top]=element;
+        }
+        else{
+            System.out.println("stack is full");
+        }
+    }
+    public void pop(){
+        if(!isEmpty()){
+            System.out.println("popped element: "+stack[top]);
+            top--;
+        }
+        else{
+            System.out.println("Stack is Empty");
+        }
+    }
+
+    public void peek(){
+        if(!isEmpty()){
+            System.out.println("top element: "+stack[top]);
+        }
+        else{
+            System.out.println("Stack is empty");
+        }
+    }
+    public void display(){
+        System.out.println("-------------------------------");
+        for(int i=0;i<=top;i++){
+            System.out.println(stack[i]);
+        }
+        System.out.println("--------------------------------");
+    }
+
+    public boolean isFull(){
+        if(top==size-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean isEmpty(){
+        if(top==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        MyStack stack = new MyStack(5);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.display();
+        stack.pop();
+        stack.pop();
+        stack.display();
+        stack.peek();
+        stack.pop();
+        stack.peek();
+
+    }
+}
